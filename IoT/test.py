141395ad68a05a3a1b166243cec6cc85053d5e46
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2018-05-17 14:46:12 +0200

    Adding IOT#1 report

--- IoT/test.py
+++ IoT/test.py
@@ -2,14 +2,16 @@ from scapy.all import *
 
 
 pcap = rdpcap("/home/luigi/Downloads/milight-remote-startup-and-on-off-just-ip.pcap")
+pcap = rdpcap("captures/c")
 
-for p in pcap[100:]:
+for p in pcap:
     if 'TCP' in p:
-        #print str(p['TCP'].payload)
-        #print p['IP'].src + "--> " + p['IP'].dst
+        payload = p['TCP'].payload
+        if not payload: continue
+
         #if p['IP'].src == '192.168.101.247':
-        #if p['IP'].src == '34.242.121.115':
-        print str(p.time) + " " + p['IP'].src + "      " + str(p['TCP'].payload).encode('hex')
+        if p['IP'].src == '34.242.121.115':
+            print "%-15s %s          %s" % (str(p.time),p['IP'].src,str(payload).encode('hex'))
 
 
 # 1 on
