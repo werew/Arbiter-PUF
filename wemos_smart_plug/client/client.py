42be0c91d60846e22af09945b5b6621268fe8c41
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2018-05-31 10:49:32 +0200

    ~HMAC authentication

--- wemos_smart_plug/client/client.py
+++ wemos_smart_plug/client/client.py
@@ -6,18 +6,27 @@ __esp8266__ = False
 import usocket
 import machine
 import ussl
+import ujson
+import urandom
+import uhashlib
+import ustruct
+import ubinascii
 if __esp8266__: import network
 
 
 
 SERVER_NAME = "192.168.150.199"
 SERVER_NAME = "0.0.0.0"
-SERVER_NAME = "192.168.2.17"
+#SERVER_NAME = "192.168.2.24"
 SERVER_PORT = 10000
 WIFI_SSID = "esp8266"
 WIFI_SSID = "Wifi282-1"
 WIFI_PSK = "twente2018"
 WIFI_PSK = "WiFi28201!"
+MAX_CHALLS = 4
+
+
+SECRET= b"43993478947239474234823942398472394732984723"
 
 
 
@@ -33,8 +42,7 @@ def connect_to_server():
     addr = addr_info[0][-1]
     sock = usocket.socket(usocket.AF_INET,  usocket.SOCK_STREAM)
     sock.connect(addr)
-    sslsock = ussl.wrap_socket(sock)
-    return sslsock
+    return sock
 
 def turn_on():
     if __esp8266__: machine.Pin(2, machine.Pin.OUT).off()
@@ -45,12 +53,72 @@ def turn_off():
     print("Off")
 
 
-if __esp8266__: connect_to_wifi()
-s = connect_to_server()
+def gen_otp(action,chall):
+    a = action.encode("utf-8")
+    c = ustruct.pack(">I",chall)
+    h = uhashlib.sha256(SECRET+a+c)
+    return ubinascii.hexlify(h.digest()).decode("utf-8")
 
-while True:
-    data = s.read(1)
-    if (data == b'0'):
-        turn_off()
-    elif (data == b'1'):
-        turn_on()
+
+class Chall_Factory:
+    def __init__(self,max_challs):
+        self.max_challs = max_challs
+        self.challs = [] 
+
+    def gen_chall(self):
+        chall = urandom.getrandbits(32)
+        self.challs.append(chall)
+        if len(self.challs) > self.max_challs:
+            self.challs.pop(0)
+
+        return chall
+
+    def verify(self,chall):
+        return chall in self.challs
+
+    def verify_and_rm(self,chall):
+        if self.verify(chall):
+            self.challs.remove(chall)
+            return True
+        return False
+
+def main():
+
+    if __esp8266__: connect_to_wifi()
+    s = connect_to_server()
+
+    cf = Chall_Factory(MAX_CHALLS)
+    while True:
+        try:
+            l = s.readline()
+            print(l)
+            if len(l) == 0: return
+            cmd = ujson.loads(l)
+        
+            if cmd['action'] == "genchall":
+                s.send('{"chall":'+str(cf.gen_chall())+'}\n')
+
+            elif cmd['action'] == "on":
+                if cf.verify_and_rm(cmd['chall']) and \
+                   gen_otp(cmd["action"],cmd["chall"]) == cmd["otp"]:
+                        turn_on()
+                else: 
+                    print("Command refused")
+
+            elif cmd['action'] == "off":
+                if cf.verify_and_rm(cmd['chall']) and \
+                   gen_otp(cmd["action"],cmd["chall"]) == cmd["otp"]:
+                        turn_off()
+                else: 
+                    print("Command refused")
+
+            else:
+                print("Invalid action")
+
+        except Exception as e:
+            print(e)
+            continue
+        
+
+main()
+
--- wemos_smart_plug/server/server.py
+++ wemos_smart_plug/server/server.py
@@ -2,24 +2,93 @@
 
 from gevent.server import StreamServer
 from gevent import sleep
-import ssl
+import json
+import hashlib
+import struct
 
 SLEEP = 0.5
 PORT = 10000
 
+SECRET = b"43993478947239474234823942398472394732984723"
+MAX_CHALLS = 4
+
+def readline(socket):
+    line = b""
+    for _ in range(1024):
+        c = socket.recv(1)
+        line += c
+        if c == b"\n": 
+            return line.decode("utf-8") 
+
+
+
+
+class Chall_Factory:
+    def __init__(self,max_challs):
+        self.max_challs = max_challs
+        self.challs = []
+
+    def gen_chall(self):
+        chall = urandom.getrandbits(32)
+        self.challs.append(chall)
+        if len(self.chall) > self.max_challs:
+            self.chall.pop(0)
+
+        return chall
+
+    def verify(self,chall):
+        return chall in self.challs
+
+    def verify_and_rm(self,chall):
+        if self.verify(chall):
+            self.challs.remove(chall)
+            return True
+        return False
+
+def gen_otp(action,chall):
+    a = action.encode("utf-8")
+    c = struct.pack(">I",chall)
+    h = hashlib.sha256(SECRET+a+c)
+    return h.hexdigest()
+
+
+    
+
+def turn_on(socket):
+    socket.sendall(b'{"action":"genchall"}\n')  
+    response = json.loads(readline(socket))
+    otp = gen_otp("on",response["chall"])
+    msg  = json.dumps({"action": "on", "chall": response["chall"], "otp": otp})+"\n"
+    socket.sendall(msg.encode("utf-8"))
+
+def turn_off(socket):
+    socket.sendall(b'{"action":"genchall"}\n')  
+    response = json.loads(readline(socket))
+    otp = gen_otp("off",response["chall"])
+    msg  = json.dumps({"action": "off", "chall": response["chall"], "otp": otp})+"\n"
+    socket.sendall(msg.encode("utf-8"))
+
+
 
 def onoff(socket, address):
+
     print('New connection from %s:%s' % address)
-    socket = ssl.wrap_socket(socket,server_side=True,certfile="cert.pem",keyfile="cert.pem")
-    try:
-        while(True):
-            socket.sendall(b'0')
+    socket.settimeout(5)
+    cf = Chall_Factory(MAX_CHALLS)
+
+    while(True):
+        try:
+            turn_on(socket)
             sleep(SLEEP)
-            socket.sendall(b'1')
+            turn_off(socket)
             sleep(SLEEP)
-    except ConnectionError:
-        print('Closing connection to %s:%s' % address)
-        socket.close()
+
+
+        except Exception as e:
+            print(e)
+            print('Closing connection to %s:%s' % address)
+            socket.close()
+            return
 
 
 if __name__ == '__main__':
